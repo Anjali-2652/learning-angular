5b83cd1d108a7e56db03819b0c9b7c8027a50603
Anjali Gupta <guptaanjali2651@gmail.com>
2026-09-07 22:40:08 +0545

    routing learned

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { About } from './about/about';
 import { Services } from './services/services';
 import { Gallery } from './gallery/gallery';
 import { Contact } from './contact/contact';
+import { Project } from './project/project';
 
 export const routes: Routes = [
     {path: "", component: Home},
     {path: "about", component: About},
-    {path: "services", component: Services},
-    {path: "gallery", component: Gallery},
-    {path: "contact", component: Contact}
+    // {path: "services", component: Services},
+    // {path: "gallery", component: Gallery},
+    // {path: "contact", component: Contact},
+    {path: "project", component: Project}
+    
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
