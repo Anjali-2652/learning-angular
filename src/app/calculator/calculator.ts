57e4283316aef955f1866569f5c66dad06e2d217
Anjali Gupta <guptaanjali2651@gmail.com>
2026-09-09 22:22:47 +0545

    calculator updated

--- src/app/calculator/calculator.ts
+++ src/app/calculator/calculator.ts
@@ -7,26 +7,61 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.css',
 })
 export class Calculator {
-  value: String = "";
+  value: String = " ";
+  num1: number | null = null;
+  operator: string | null = null;
+  num2Waiting: boolean = false
 
-appendNumber(input: string){
-this.value += input ;
+appendNumber(num: string){
+// this.value += num ;
+if(this.num2Waiting){
+  this.value = num;
+  this.num2Waiting = false;
+}else{
+  this.value = this.value === "" ? num: this.value + num;
+}
 }
 
-clear(): void{
-  this.value = ""
+pressOperator(op:string){
+  this.num1 = Number(this.value);
+  this.operator = op;
+  this.num2Waiting = true;
+}
+
+clear(){
+  this.value = "";
+  this.num1 = null;
+  this.operator = null;
+  this.num2Waiting = false;
 }
 
 delete(): void{
   this.value = this.value.slice(0,-1)
 }
 
-calculate(): void{
-  try{
-    this.value = eval(this.value as string).toString();
-  }catch{
-    this.value = "Error"
-  }
+calculate(){
+if(this.num1 === null || this.operator === null) return;
+const num2 = Number(this.value);
+let result  = 0;
+switch(this.operator){
+  case "+":
+    result = this.num1 + num2;
+    break;
+  case "-":
+    result = this.num1 - num2;
+    break;
+  case "*":
+    result = this.num1 * num2;
+    break;
+  case "/":
+    result = this.num1 / num2;
+    break;
+  case "%":
+    result = this.num1 % num2;
+    break;
 }
-
-}
+this.value = result.toString();
+this.num1 = null;
+this.operator = null;
+} 
+} 
