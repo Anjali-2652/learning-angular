ce521bcde62b9cfdefec095ba53e03e0d9aafe7f
Anjali Gupta <guptaanjali2651@gmail.com>
2026-09-12 22:12:03 +0545

    register form updated

--- src/app/reactive-forms/reactive-forms.ts
+++ src/app/reactive-forms/reactive-forms.ts
@@ -1,20 +1,21 @@
-import { NgStyle } from '@angular/common';
+import { NgStyle, NgClass, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-reactive-forms',
-  imports: [ReactiveFormsModule,NgStyle],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-forms.html',
   styleUrl: './reactive-forms.css',
 })
 export class ReactiveForms {
   isActive:boolean = false;
+  selectedRole: 'employee' | 'hirer' = 'employee'
 
-  changeState(){
-    this.isActive = !this.isActive
-  }
+changeState(){
+  this.isActive = !this.isActive
+}
 form = new FormGroup({
   firstName: new FormControl('', [Validators.required]),
   email: new FormControl('', [Validators.required, Validators.email])
