1395ca8e3f93c371bf477b15df037c6da6b2da24
Anjali Gupta <guptaanjali2651@gmail.com>
2026-09-13 12:28:23 +0545

    custome pipe

--- src/app/project/project.ts
+++ src/app/project/project.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReversePipe } from '../reverse-pipe';
 
 @Component({
   selector: 'app-project',
-  imports: [CommonModule],
+  imports: [CommonModule,ReversePipe ],
   templateUrl: './project.html',
   styleUrl: './project.css',
 })
