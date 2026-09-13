b720fd4090c005f42bd68411b916daf5ea9a3f45
Anjali Gupta <guptaanjali2651@gmail.com>
2026-09-13 23:05:41 +0545

    register form updated validation logic

--- src/app/reactive-forms/reactive-forms.ts
+++ src/app/reactive-forms/reactive-forms.ts
@@ -1,10 +1,13 @@
 import { NgStyle, NgClass, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators , 
+  FormGroup
+} from '@angular/forms';
 import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-reactive-forms',
+  standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-forms.html',
   styleUrl: './reactive-forms.css',
@@ -16,12 +19,102 @@ export class ReactiveForms {
 changeState(){
   this.isActive = !this.isActive
 }
-form = new FormGroup({
-  firstName: new FormControl('', [Validators.required]),
-  email: new FormControl('', [Validators.required, Validators.email])
-});
 
-info(){
-  alert("email id :"+( this.form.value.email))
+// for registration logic .. 
+employeeForm : FormGroup;
+hirerForm: FormGroup;
+
+constructor(private fb: FormBuilder){
+  this.employeeForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required, Validators.email],
+    phoneNo: ['', Validators.required],
+    gender: ['', Validators.required],
+    address: ['', Validators.required],
+    password: ['', Validators.required, Validators.minLength(6)],
+    confirmPassword: ['', Validators.required],
+  });
+
+  this.hirerForm = this.fb.group({
+    complanyName: ['', Validators.required],
+    contactPerson: ['', Validators.required],
+    email: ['', Validators.required, Validators.email],
+    phoneNo: ['', Validators.required],
+    gender: ['', Validators.required],
+    address: ['', Validators.required],
+    password: ['', Validators.required, Validators.minLength(6)],
+    confirmPassword: ['', Validators.required],
+  })
+}
+
+selectEmployee(): void{
+  this.selectedRole = 'employee'
+}
+
+selectHirer(): void{
+  this.selectedRole = 'hirer'
+}
+
+// check if form is valid .. 
+registerEmployee(): void{
+  if(this.employeeForm.invalid){
+    this.employeeForm.markAllAsTouched();
+    return;
+  }
+
+  // get form data .. 
+  const employeeData = this.employeeForm.value;
+  console.log('Employee Registered: ')
+  console.log(employeeData);
+}
+
+registerHirer(): void{
+  if(this.hirerForm.invalid){
+    this.hirerForm.markAllAsTouched();
+    return;
+  }
+
+  // get form data .. 
+  const hirerData = this.hirerForm.value;
+  console.log("hirer registered: ")
+  console.log(hirerData)
+}
+
+
+// employee form validation.. 
+employeeFieldInvalid(fieldName: string): boolean {
+  const field = this.employeeForm.get(fieldName)
+
+  return !!(
+    field &&
+    field.invalid &&
+    field.touched
+  );
+}
+
+// hirer form validation .. 
+hirerFieldInvalid(fieldName: string): boolean {
+  const field = this.hirerForm.get(fieldName)
+
+  return !!(
+    field &&
+    field.invalid &&
+    field.touched
+  );
+}
+
+employeePasswordMatch(): boolean{
+  const password = this.employeeForm.get('password')?.value;
+  
+  const confirmPassword =  this.employeeForm.get('confirmPassword')?.value;
+  return password === confirmPassword
+}
+
+hirerPasswordMatch(): boolean{
+  const password = this.hirerForm.get('password')?.value;
+  
+  const confirmPassword =  this.hirerForm.get('confirmPassword')?.value;
+  return password === confirmPassword
 }
 }
